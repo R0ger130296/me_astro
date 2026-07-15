9a7099bb57bff77a610c056941a01b1b9057200c
Roger <40442199+R0ger130296@users.noreply.github.com>
2026-07-15 13:58:19 -0500

    chore: remove Astro DB from main

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
-import db from '@astrojs/db';
 
 export default defineConfig({
   site: 'https://rogercedeno.dev',
@@ -12,7 +11,6 @@ export default defineConfig({
       applyBaseStyles: false,
       nesting: true,
     }),
-    db(),
   ],
   output: 'server',
   adapter: vercel(),
